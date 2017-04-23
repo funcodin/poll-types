dd3b6fb7f7f5345973f963cc8e8f2db4ab29ad74
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-04-23 00:27:38 -0700

    Added user reset type

---
+++ src/main/java/com/multi/enterprise/types/poll/accounts/UserReset.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.multi.enterprise.types.poll.accounts;
+
+/**
+ * @author Robot
+ *
+ */
+public class UserReset {
+
+	private String userName;
+	private String email;
+	private String responseString;
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the responseString
+	 */
+	public String getResponseString() {
+		return responseString;
+	}
+
+	/**
+	 * @param responseString the responseString to set
+	 */
+	public void setResponseString(String responseString) {
+		this.responseString = responseString;
+	}
+
+}
